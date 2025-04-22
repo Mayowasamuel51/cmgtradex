bb1021307f49f46fb4f779117eb46a477936a48f
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-04-22 03:52:00 +0100

    home page done

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -243,54 +243,11 @@
         <div class="row">
             <div class="col-12">
                 <div class="section-title">
-                    <h2>Hot Items</h2>
+                  
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-12">
-                <div class="owl-carousel popular-slider">
-                    @foreach($product_lists as $product)
-                    @if($product->condition=='hot')
-                    <!-- Start Single Product -->
-                    <div class="single-product">
-                        <div class="product-img">
-                            <!-- <a href="{{route('product-detail',$product->slug)}}">
-                                    @php
-                                        $photo=explode(',',$product->photo);
-                                    // dd($photo);
-                                    @endphp
-                                    <img class="default-img" src="{{$photo[0]}}" alt="{{$photo[0]}}">
-                                    <img class="hover-img" src="{{$photo[0]}}" alt="{{$photo[0]}}">
-                                    {{-- <span class="out-of-stock">Hot</span> --}}
-                                </a> -->
-                            <div class="button-head">
-                                <div class="product-action">
-                                    <a data-toggle="modal" data-target="#{{$product->id}}" title="Quick View" href="#"><i class=" ti-eye"></i><span>Quick Shop</span></a>
-                                    <a title="Wishlist" href="{{route('add-to-wishlist',$product->slug)}}"><i class=" ti-heart "></i><span>Add to Wishlist</span></a>
-                                </div>
-                                <div class="product-action-2">
-                                    <a href="{{route('add-to-cart',$product->slug)}}">Add to cart</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-content">
-                            <h3><a href="{{route('product-detail',$product->slug)}}">{{$product->title}}</a></h3>
-                            <div class="product-price">
-                                <span class="old">${{number_format($product->price,2)}}</span>
-                                @php
-                                $after_discount=($product->price-($product->price*$product->discount)/100)
-                                @endphp
-                                <span>${{number_format($after_discount,2)}}</span>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Product -->
-                    @endif
-                    @endforeach
-                </div>
-            </div>
-        </div>
+       
     </div>
 </div>
 <!-- End Most Popular Area -->
@@ -329,7 +286,7 @@
                                 <div class="col-lg-6 col-md-6 col-12 no-padding">
                                     <div class="content">
                                         <h4 class="title"><a href="#">{{$product->title}}</a></h4>
-                                        <p class="price with-discount">${{number_format($product->discount,2)}}</p>
+
                                     </div>
                                 </div>
                             </div>
