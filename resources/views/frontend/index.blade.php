76878c44a8c627cdb9312f4f95ba6cf8aabc428f
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-04-22 14:17:40 +0100

    i rmove the banner single

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -94,17 +94,7 @@
             @if($cat->is_parent==1)
             <!-- Single Banner  -->
             <div class="col-lg-4 col-md-6 col-12">
-                <div class="single-banner">
-                    @if($cat->photo)
-                    <img src="{{$cat->photo}}" alt="{{$cat->photo}}">
-                    @else
-                    <img src="https://via.placeholder.com/600x370" alt="#">
-                    @endif
-                    <div class="content">
-                        <h3 style="color:white; text-shadow: 2px 2px 5px red;">{{$cat->title}}</h3>
-                        <a href="{{route('product-cat',$cat->slug)}}" style="color:white; text-shadow: 2px 2px 5px red;">Discover Now</a>
-                    </div>
-                </div>
+             
             </div>
             @endif
             <!-- /End Single Banner  -->
