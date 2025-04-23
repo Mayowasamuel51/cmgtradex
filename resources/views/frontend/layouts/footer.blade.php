8bbf8f504208539884b03257be6572b07104dfb3
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-04-23 13:09:41 +0100

    newwn

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -39,8 +39,8 @@
 							<h4>Customer Service</h4>
 							<ul>
 								<li><a href="#">Payment Methods</a></li>
-								<li><a href="#">Money-back</a></li>
-								<li><a href="#">Returns</a></li>
+								<!-- <li><a href="#">Money-back</a></li> -->
+								<!-- <li><a href="#">Returns</a></li> -->
 								<li><a href="#">Shipping</a></li>
 								<li><a href="#">Privacy Policy</a></li>
 							</ul>
